91a0ae0b311b948496bcf6ad1b3c83af896dd8e4
Alice Dumaresq <144940625+Adumaresq@users.noreply.github.com>
2024-12-16 17:29:38 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,26 @@
+package atividade;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+  // Construtor
+
+  //Gett
+  public String getNome() {
+    return nome;
+  }
+
+public Pessoa(String nome, int idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+
+public int getIdade() {
+return idade;
+}
+
+//Metodo info 
+public void exibirInfo() {
+System.out.println("nome:" + nome + "| idade:" + idade);
+}
